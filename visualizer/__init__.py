25cbc6639f1445039fc52225f275f6bde9bd284c
Pulin Agrawal <pulinagrawal@gmail.com>
2026-01-08 17:50:29 -0500

    Remove obsolete visualizer components and related classes

--- visualizer/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Interactive visualization helpers for HTM building blocks."""
-
-from .simulator import SimulationConfig, SimulationResult, run_simulation
-
-__all__ = ["SimulationConfig", "SimulationResult", "run_simulation"]
